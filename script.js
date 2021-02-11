281f7d6e5acaf0d55367cb1fe70b6ce6900da24c
BatuhanB <56514839+BatuhanB@users.noreply.github.com>
2021-02-11 21:20:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const Year = "7 March 2021";
+const Year = "31 August 2021";
 
 function countdown() {
     const YearDate = new Date(Year);
